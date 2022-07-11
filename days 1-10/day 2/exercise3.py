5ae8c4e92f3888f5617ea05388aa818991474885
[ishkining] <ishkining@gmail.com>
2022-07-11 14:28:19 +0500

    commit second day challanges

---
+++ days 1-10/day 2/exercise3.py
@@ -0,0 +1,7 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+how_many_years_you_have = 90 - int(age)
+print(f'you have {how_many_years_you_have * 365} days in a year, {how_many_years_you_have * 52} weeks in a year and {how_many_years_you_have * 12} months in a year')

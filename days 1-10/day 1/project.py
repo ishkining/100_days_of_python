8a597b8e51de340c2b0cfccf34288a1176a624ab
[ishkining] <ishkining@gmail.com>
2022-07-11 12:35:20 +0500

    commit first day challanges

---
+++ days 1-10/day 1/project.py
@@ -0,0 +1,10 @@
+# 1. Create a greeting for your program.
+print('Greetings, my lord!')
+# 2. Ask the user for the city that they grew up in.
+lands = input('In which lands are you living, sir?\n')
+# 3. Ask the user for the name of a pet.
+pet_name = input('What is the name of your pet, sir?\n')
+# 4. Combine the name of their city and pet and show them their band name.
+print('We have a cool band name for you, sir - ' + lands + ' ' + pet_name)
+# 5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
